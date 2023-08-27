e75bb0b397026fe082f670d7f3aa0ad41c4bb14a
Vladimir <vladimirbashev@gmail.com>
2023-08-27 20:24:50 +0200

    set up

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AuthModule} from "./auth/auth.module";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/app/auth/auth.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RegisterComponent } from './register/register.component';
+import {RouterModule} from "@angular/router";
+
+
+const routes = [
+  {
+    path: 'register',
+    component: RegisterComponent
+  }
+]
+
+@NgModule({
+  declarations: [
+    RegisterComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes)
+  ]
+})
+export class AuthModule { }
