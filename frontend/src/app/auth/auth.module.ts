5a1ae0567ad17618f059da2f247711a4f1fb07db
Vladimir <vladimirbashev@gmail.com>
2023-08-27 20:47:00 +0200

    move components

--- frontend/src/app/auth/auth.module.ts
+++ frontend/src/app/auth/auth.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RegisterComponent } from './register/register.component';
+import { RegisterComponent } from './components/register/register.component';
 import {RouterModule} from "@angular/router";
 
 
