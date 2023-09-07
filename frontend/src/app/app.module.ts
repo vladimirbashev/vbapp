e7a78bc03b5f39c25ef3e152d679450536d8ac21
Vladimir <vladimirbashev@gmail.com>
2023-09-07 17:16:57 +0200

    refactor

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {AuthModule} from "./auth/auth.module";
-import {FlexLayoutModule} from "@angular/flex-layout";
 
 @NgModule({
   declarations: [
--- frontend/src/app/auth/auth.module.ts
+++ frontend/src/app/auth/auth.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RegisterComponent } from './components/register/register.component';
 import {RouterModule} from "@angular/router";
 import {ReactiveFormsModule} from "@angular/forms";
 import {FlexLayoutModule} from "@angular/flex-layout";
 import {MatButtonModule} from "@angular/material/button";
 import {MatInputModule} from "@angular/material/input";
 
+import { RegisterComponent } from './components/register/register.component';
 
 const routes = [
   {
