1254c90ca2b8d942e652cc297cf856aaf49346a6
Vladimir <vladimirbashev@gmail.com>
2023-09-08 18:51:23 +0200

    store

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import {AuthModule} from "./auth/auth.module";
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import {AuthModule} from "./auth/auth.module";
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    AuthModule
+    AuthModule,
+    StoreModule.forRoot({}, {})
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/auth/components/register/register.component.ts
+++ frontend/src/app/auth/components/register/register.component.ts
@@ -1,5 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Store} from '@ngrx/store'
+
+import {registerAction} from "../../store/actions/register.actions";
 
 @Component({
   selector: 'app-register',
@@ -9,7 +12,7 @@ import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 export class RegisterComponent implements OnInit {
   form: FormGroup
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder, private store: Store) {}
 
   ngOnInit(): void {
     this.initializeForm()
@@ -26,7 +29,8 @@ export class RegisterComponent implements OnInit {
 
   onSubmit(): void {
     if (this.form.valid) {
-      console.log('submit', this.form.value, this.form.valid)
+      console.log('submit', this.form.value, this.form.valid);
+      this.store.dispatch(registerAction(this.form.value));
     }
   }
 }
