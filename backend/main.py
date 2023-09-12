cd56c899b5cbd213e10b84d824a8b2d5482c6449
Vladimir <vladimirbashev@gmail.com>
2023-09-12 20:14:41 +0200

    api url

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,8 @@ from routes import users, items
 
 users_models.Base.metadata.create_all(bind=engine)
 items_models.Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
-app.include_router(users.router)
-app.include_router(items.router)
+app.include_router(users.router, prefix='/api')
+app.include_router(items.router, prefix='/api')
 
