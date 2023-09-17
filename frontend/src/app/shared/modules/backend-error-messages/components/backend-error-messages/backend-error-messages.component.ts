334b745232ca7de74d577562ef28df8cb321f53d
Vladimir <vladimirbashev@gmail.com>
2023-09-17 19:33:05 +0200

    errors

--- frontend/src/app/shared/modules/backend-error-messages/components/backend-error-messages/backend-error-messages.component.ts
+++ frontend/src/app/shared/modules/backend-error-messages/components/backend-error-messages/backend-error-messages.component.ts
@@ -1,14 +1,22 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {Observable} from "rxjs";
+import {map} from "rxjs/operators";
 
 @Component({
   selector: 'app-backend-error-messages',
   templateUrl: './backend-error-messages.component.html',
   styleUrls: ['./backend-error-messages.component.scss']
 })
-export class BackendErrorMessagesComponent {
-  @Input('value') value: any
+export class BackendErrorMessagesComponent implements OnInit{
+  @Input('value') value: Observable<any>
+  message: Observable<string>
 
-  getMessage() {
-    return this.value && typeof this.value !== 'string' ? 'Internal error' : this.value
+  ngOnInit(): void {
+    this.message = this.value.pipe(
+      map((value) => {
+        return value && typeof value !== 'string' ? 'Internal error' : value
+      })
+    )
   }
+
 }
