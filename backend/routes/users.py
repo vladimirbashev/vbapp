ca133a8093082d2389c26ce14650fab8831f9477
Vladimir <vladimirbashev@gmail.com>
2023-09-22 19:49:55 +0200

    fix

--- backend/routes/users.py
+++ backend/routes/users.py
@@ -13,11 +13,15 @@ router = APIRouter()
 # user: SystemUser = Depends(get_current_user)
 # current_user: Annotated[User, Depends(get_current_user)]
 
+
 @router.post("/users/", response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_email(db, email=user.email)
     if db_user:
         raise HTTPException(status_code=400, detail="Email already registered")
+    db_user = crud.get_user_by_username(db, username=user.username)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Username already registered")
     return crud.create_user(db=db, user=user)
 
 
