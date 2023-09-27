ed143aded1f6eda0860471d8aa7a97a1bb396ef8
Vladimir <vladimirbashev@gmail.com>
2023-09-27 21:09:02 +0200

    docker-compose

--- backend/auth/utils.py
+++ backend/auth/utils.py
@@ -8,7 +8,6 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30  # 30 minutes
 REFRESH_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7  # 7 days
 ALGORITHM = "HS256"
 JWT_SECRET_KEY = os.environ['JWT_SECRET_KEY']  # should be kept secret
-JWT_REFRESH_SECRET_KEY = os.environ['JWT_REFRESH_SECRET_KEY']  # should be kept secret
 
 password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
